b2ce60b646a8abbe71be2b53d52fcedb44870437
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-18 15:28:45 +0200

    Set up firestore database

---
+++ thesis-experiment/src/utils/db.ts
@@ -0,0 +1,15 @@
+import { collection, addDoc } from 'firebase/firestore'
+import db from './firebaseInit'
+
+/**
+ * Add data to a Firestore collection
+ * @param table Firestore collection name
+ * @param data Data to add
+ */
+export async function addData(table: string, data: unknown): Promise<void> {
+  const docRef = await addDoc(collection(db, table), data)
+
+  if (docRef === null) {
+    throw new Error('An error occurred while adding the data')
+  }
+}

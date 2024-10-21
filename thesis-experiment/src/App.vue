11e61a5e4ed308c76f5be644cbe39a90a8b226e7
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-21 16:47:38 +0200

    Select text styling

--- thesis-experiment/src/App.vue
+++ thesis-experiment/src/App.vue
@@ -20,6 +20,11 @@
       </li>
     </ul>
   </div>
+  <p class="select-text">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro suscipit
+    explicabo sapiente similique expedita culpa beatae magni eveniet alias ad!
+    Corrupti modi atque eos, optio dolorem aspernatur rem molestiae animi.
+  </p>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'

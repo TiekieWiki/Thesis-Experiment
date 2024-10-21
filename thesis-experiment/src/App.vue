a4d4b4f5776bbb8d7abe14b650a714eaa72ad0de
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-21 16:39:19 +0200

    Menu styling

--- thesis-experiment/src/App.vue
+++ thesis-experiment/src/App.vue
@@ -1,5 +1,29 @@
 <template>
   <a href="#">Test</a
   ><img src="./assets/images/Test.jpg" alt="Photo by Greg Becker on Unsplash" />
+  <div class="menu">
+    <transition name="switch" mode="out-in">
+      <font-awesome-icon
+        v-if="!menuOpen"
+        @click="menuOpen = true"
+        :icon="['fas', 'bars']"
+      />
+      <font-awesome-icon
+        v-else
+        @click="menuOpen = false"
+        :icon="['fas', 'xmark']"
+      />
+    </transition>
+    <ul v-if="menuOpen">
+      <li v-for="item in menuItems" :key="item">
+        <a href="#">{{ item }}</a>
+      </li>
+    </ul>
+  </div>
 </template>
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const menuOpen = ref<boolean>(false)
+const menuItems: string[] = ['A', 'B', 'C']
+</script>
--- thesis-experiment/src/main.ts
+++ thesis-experiment/src/main.ts
@@ -1,11 +1,14 @@
 import './assets/styles/main.scss'
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { far } from '@fortawesome/free-regular-svg-icons'
+library.add(fas, far)
 
-const app = createApp(App)
-
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(router)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app')

7d7401a08eb330018e5220bb34e43df64133a2ca
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-21 16:14:09 +0200

    Picture styling

--- thesis-experiment/src/App.vue
+++ thesis-experiment/src/App.vue
@@ -1,2 +1,5 @@
-<template><a href="#">Test</a></template>
+<template>
+  <a href="#">Test</a
+  ><img src="./assets/images/Test.jpg" alt="Photo by Greg Becker on Unsplash" />
+</template>
 <script setup lang="ts"></script>

e2744cc732efc2f251b3e2f4f1103abb46d263cb
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-26 15:36:53 +0200

    List styling

--- thesis-experiment/src/App.vue
+++ thesis-experiment/src/App.vue
@@ -57,12 +57,30 @@
     <div class="slider">
       <input type="range" min="1" max="100" value="1" />
     </div>
+    <draggable
+      v-model="dragList"
+      @start="drag = true"
+      @end="drag = false"
+      item-key="id"
+      class="list"
+    >
+      <template #item="{ element }">
+        <div class="list-item">{{ element.name }}</div>
+      </template>
+    </draggable>
   </main>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'
+import draggable from 'vuedraggable'
 
 const menuOpen = ref<boolean>(false)
 const menuItems: string[] = ['A', 'B', 'C']
 const popUpOpen = ref<boolean>(false)
+const drag = ref<boolean>(false)
+const dragList = ref<{ id: number; name: string }[]>([
+  { id: 1, name: 'A' },
+  { id: 2, name: 'B' },
+  { id: 3, name: 'C' },
+])
 </script>

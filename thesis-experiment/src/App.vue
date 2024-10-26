66c6223b8854b2382ca7d077a9b6c95cde251c3d
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-26 12:44:33 +0200

    Form styling

--- thesis-experiment/src/App.vue
+++ thesis-experiment/src/App.vue
@@ -1,39 +1,60 @@
 <template>
-  <a href="#">Test</a
-  ><img src="./assets/images/Test.jpg" alt="Photo by Greg Becker on Unsplash" />
-  <div class="menu">
-    <transition name="switch" mode="out-in">
-      <button v-if="!menuOpen" @click="menuOpen = true">
-        <font-awesome-icon :icon="['fas', 'bars']" />
-        <p>Menu</p>
-      </button>
-      <button v-else>
-        <font-awesome-icon @click="menuOpen = false" :icon="['fas', 'xmark']" />
-        <p>Menu</p>
-      </button>
-    </transition>
-    <ul v-if="menuOpen">
-      <li v-for="item in menuItems" :key="item">
-        <a href="#">{{ item }}</a>
-      </li>
-    </ul>
-  </div>
-  <p class="select-text">
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro suscipit
-    explicabo sapiente similique expedita culpa beatae magni eveniet alias ad!
-    Corrupti modi atque eos, optio dolorem aspernatur rem molestiae animi.
-  </p>
-  <button @click="popUpOpen = true">Open pop-up</button>
-  <teleport to="body" v-if="popUpOpen">
-    <div class="pop-up-background">
-      <div class="pop-up">
-        <button @click="popUpOpen = false">
-          <font-awesome-icon :icon="['fas', 'times']" />
+  <main>
+    <a href="#">Test</a
+    ><img
+      src="./assets/images/Test.jpg"
+      alt="Photo by Greg Becker on Unsplash"
+    />
+    <div class="menu">
+      <transition name="switch" mode="out-in">
+        <button v-if="!menuOpen" @click="menuOpen = true">
+          <font-awesome-icon :icon="['fas', 'bars']" />
+          <p>Menu</p>
         </button>
-        <p>Content</p>
-      </div>
+        <button v-else>
+          <font-awesome-icon
+            @click="menuOpen = false"
+            :icon="['fas', 'xmark']"
+          />
+          <p>Menu</p>
+        </button>
+      </transition>
+      <ul v-if="menuOpen">
+        <li v-for="item in menuItems" :key="item">
+          <a href="#">{{ item }}</a>
+        </li>
+      </ul>
     </div>
-  </teleport>
+    <p class="select-text">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro suscipit
+      explicabo sapiente similique expedita culpa beatae magni eveniet alias ad!
+      Corrupti modi atque eos, optio dolorem aspernatur rem molestiae animi.
+    </p>
+    <button @click="popUpOpen = true">Open pop-up</button>
+    <teleport to="body" v-if="popUpOpen">
+      <div class="pop-up-background">
+        <div class="pop-up">
+          <button @click="popUpOpen = false">
+            <font-awesome-icon :icon="['fas', 'times']" />
+          </button>
+          <p>Content</p>
+        </div>
+      </div>
+    </teleport>
+    <form>
+      <label for="">Test <input type="text" placeholder="test" /> </label>
+      <div class="label-group">
+        <label for=""><input type="radio" name="" id="" />Test</label>
+      </div>
+      <div class="select">
+        <select name="" id="">
+          <option value="">Test</option>
+        </select>
+      </div>
+      <p class="error">Test</p>
+      <p class="success">Test</p>
+    </form>
+  </main>
 </template>
 <script setup lang="ts">
 import { ref } from 'vue'

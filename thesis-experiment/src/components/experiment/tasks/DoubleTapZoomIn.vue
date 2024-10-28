b7a26ef1ce694945a0ea8a7dd952d1c2c0fae478
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-28 12:13:05 +0100

    Moved html to correct components

---
+++ thesis-experiment/src/components/experiment/tasks/DoubleTapZoomIn.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="./assets/images/Test.jpg" alt="Photo by Greg Becker on Unsplash" />
+</template>
---
+++ thesis-experiment/src/components/start/DemoGraphicQuestions.vue
@@ -0,0 +1,15 @@
+<template>
+  <form>
+    <label for="">Test <input type="text" placeholder="test" /> </label>
+    <div class="label-group">
+      <label for=""><input type="radio" name="" id="" />Test</label>
+    </div>
+    <div class="select">
+      <select name="" id="">
+        <option value="">Test</option>
+      </select>
+    </div>
+    <p class="error">Test</p>
+    <p class="success">Test</p>
+  </form>
+</template>

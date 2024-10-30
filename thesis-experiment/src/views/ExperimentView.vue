33a01beb536707c5ddf52d9035c1e4c76e31c4f8
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-30 18:56:12 +0100

    Orientation mixin

--- thesis-experiment/src/views/ExperimentView.vue
+++ thesis-experiment/src/views/ExperimentView.vue
@@ -1,11 +1,11 @@
 <template>
   <main>
-    <LongTapSelect :interface-orientation="interfaceOrientation" />
+    <MultiTouchRotate :interface-orientation="interfaceOrientation" />
   </main>
 </template>
 
 <script setup lang="ts">
-import LongTapSelect from '@/components/experiment/tasks/LongTapSelect.vue'
+import MultiTouchRotate from '@/components/experiment/tasks/MultiTouchRotate.vue'
 import { ref } from 'vue'
 
 const interfaceOrientation = ref<string>('standard')

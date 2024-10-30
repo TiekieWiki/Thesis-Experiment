01701f215a757e277a4d1bded49120445def2c1e
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-30 17:47:34 +0100

    Picture components styling

--- thesis-experiment/src/components/experiment/tasks/MultiTouchRotate.vue
+++ thesis-experiment/src/components/experiment/tasks/MultiTouchRotate.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="multi-touch-rotate">
+  <div :class="['multi-touch-rotate', interfaceOrientation]">
     <img
       src="../../../assets/images/Test.jpg"
       alt="Photo by Greg Becker on Unsplash"
     />
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  interfaceOrientation: string
+}>()
+</script>
--- thesis-experiment/src/components/experiment/tasks/MultiTouchZoom.vue
+++ thesis-experiment/src/components/experiment/tasks/MultiTouchZoom.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="multi-touch-zoom">
+  <div :class="['multi-touch-zoom', interfaceOrientation]">
     <img
       src="../../../assets/images/Test.jpg"
       alt="Photo by Greg Becker on Unsplash"
     />
   </div>
 </template>
+
+<script setup lang="ts">
+defineProps<{
+  interfaceOrientation: string
+}>()
+</script>

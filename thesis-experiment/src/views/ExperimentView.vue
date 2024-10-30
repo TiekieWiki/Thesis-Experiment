63791d2ba4850f2f82e70bfd4a59ff3d7827119b
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-30 17:43:51 +0100

    Double tap zoom out styling

--- thesis-experiment/src/views/ExperimentView.vue
+++ thesis-experiment/src/views/ExperimentView.vue
@@ -1,11 +1,11 @@
 <template>
   <main>
-    <DoubleTapZoomIn :interface-orientation="interfaceOrientation" />
+    <DoubleTapZoomOut :interface-orientation="interfaceOrientation" />
   </main>
 </template>
 
 <script setup lang="ts">
-import DoubleTapZoomIn from '@/components/experiment/tasks/DoubleTapZoomIn.vue'
+import DoubleTapZoomOut from '@/components/experiment/tasks/DoubleTapZoomIn.vue'
 import { ref } from 'vue'
 
 const interfaceOrientation = ref<string>('standard')

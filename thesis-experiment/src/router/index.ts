059402b28ca2365bab50979ddc5cf7d4b4a8be29
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-30 17:03:33 +0100

    Created routing

--- thesis-experiment/src/router/index.ts
+++ thesis-experiment/src/router/index.ts
@@ -2,7 +2,23 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'Start',
+      component: () => import('../views/StartView.vue'),
+    },
+    {
+      path: '/experiment',
+      name: 'Experiment',
+      component: () => import('../views/ExperimentView.vue'),
+    },
+    {
+      path: '/finishing-up',
+      name: 'End',
+      component: () => import('../views/EndView.vue'),
+    },
+  ],
 })
 
 export default router

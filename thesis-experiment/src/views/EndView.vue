1fe1d939c8c7124a271bd74ecbd80cb151fe6e3c
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-31 13:33:58 +0100

    User experience test styling

--- thesis-experiment/src/views/EndView.vue
+++ thesis-experiment/src/views/EndView.vue
@@ -1,1 +1,9 @@
-<template>End</template>
+<template>
+  <main class="end">
+    <UserExperienceTest />
+  </main>
+</template>
+
+<script setup lang="ts">
+import UserExperienceTest from '@/components/end/UserExperienceTest.vue'
+</script>

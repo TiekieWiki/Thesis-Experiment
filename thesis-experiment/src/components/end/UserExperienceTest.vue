ace24850d326b1c9fb999d57d0f2bf69b38a0557
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-10-31 13:33:58 +0100

    User experience test styling

---
+++ thesis-experiment/src/components/end/UserExperienceTest.vue
@@ -0,0 +1,32 @@
+<template>
+  <form class="user-experience-test">
+    <h1>User experience test</h1>
+    <div class="question">
+      <p>This system's capabilities meet my requirements</p>
+      <div class="label-group">
+        <label><input type="radio" />Strongly disagree</label>
+        <label><input type="radio" />Disagree</label>
+        <label><input type="radio" />Somewhat disagree</label>
+        <label><input type="radio" />Neutral</label>
+        <label><input type="radio" />Somewhat agree</label>
+        <label><input type="radio" />Agree</label>
+        <label><input type="radio" />Strongly agree</label>
+      </div>
+    </div>
+    <div class="question">
+      <p>This system is easy to use</p>
+      <div class="label-group">
+        <label><input type="radio" />Strongly disagree</label>
+        <label><input type="radio" />Disagree</label>
+        <label><input type="radio" />Somewhat disagree</label>
+        <label><input type="radio" />Neutral</label>
+        <label><input type="radio" />Somewhat agree</label>
+        <label><input type="radio" />Agree</label>
+        <label><input type="radio" />Strongly agree</label>
+      </div>
+    </div>
+    <p class="error">Test</p>
+    <p class="success">Test</p>
+    <button type="submit">Continue</button>
+  </form>
+</template>

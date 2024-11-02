8bafb1fbd384d788353d11992efa8eaa87b4dbe1
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-11-02 13:11:35 +0100

    Created task types and task select function

---
+++ thesis-experiment/src/utils/types/tasks.ts
@@ -0,0 +1,21 @@
+export const taskSet = [
+  'LeftStandard',
+  'RightStandard',
+  'LeftMirrored',
+  'RightMirrored',
+]
+
+export const tasks = [
+  'DoubleTapZoomIn',
+  'DoubleTapZoomOut',
+  'DragList',
+  'DragSlider',
+  'FlickLeft',
+  'FlickRight',
+  'LongTapHyperlink',
+  'LongTapSelect',
+  'MultiTouchRotate',
+  'MultiTouchZoom',
+  'TapMenu',
+  'TapType',
+]

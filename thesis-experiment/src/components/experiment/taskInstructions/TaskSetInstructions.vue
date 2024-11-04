f1ddbc030b3c13d8b0b656122be2f4d18b2128be
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-11-04 16:07:10 +0100

    Task set instructions based on hand

--- thesis-experiment/src/components/experiment/taskInstructions/TaskSetInstructions.vue
+++ thesis-experiment/src/components/experiment/taskInstructions/TaskSetInstructions.vue
@@ -1,4 +1,6 @@
 <template>
+  <h1 v-if="hand == 'left'">Perform tasks only with left hand</h1>
+  <h1 v-else>Perform tasks only with right hand</h1>
   <button @click="next">Next</button>
 </template>
 

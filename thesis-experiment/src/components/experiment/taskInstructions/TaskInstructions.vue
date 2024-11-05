450fc7664bbe5b526a5be5a48025623dfca70278
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-11-05 16:34:37 +0100

    Task instruction styling

--- thesis-experiment/src/components/experiment/taskInstructions/TaskInstructions.vue
+++ thesis-experiment/src/components/experiment/taskInstructions/TaskInstructions.vue
@@ -1,8 +1,17 @@
 <template>
-  <h1>Instructions</h1>
-  <p>{{ getTaskInstruction(task) }}</p>
-  <p>Reminder: perform this task with your {{ hand }} hand</p>
-  <button @click="next">Next</button>
+  <teleport to="body">
+    <div class="task-instructions pop-up-background" @click="next">
+      <div class="pop-up">
+        <h1>Instructions</h1>
+        <p>{{ getTaskInstruction(task) }}</p>
+        <p>Reminder: perform this task with your {{ hand }} hand</p>
+        <p>
+          Hold the phone in a neutral position and click this pop-up to start
+          the task
+        </p>
+      </div>
+    </div>
+  </teleport>
 </template>
 
 <script setup lang="ts">

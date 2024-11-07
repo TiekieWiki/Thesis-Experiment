d56b17f1393d269880922172e55e1f0ee7580099
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-11-07 15:21:20 +0100

    Shortened laterality test code

--- thesis-experiment/src/components/start/LateralityTest.vue
+++ thesis-experiment/src/components/start/LateralityTest.vue
@@ -1,125 +1,120 @@
 <template>
   <form class="laterality-test">
     <h1>Laterality test</h1>
-    <div class="question">
-      <p>Which hand do you use to write?</p>
+    <div
+      v-for="question in lateralityQuestions"
+      :key="question.name"
+      class="question"
+    >
+      <p>{{ question.question }}</p>
       <div class="label-group">
-        <label><input type="radio" />Left hand</label>
-        <label><input type="radio" />No preference</label>
-        <label><input type="radio" />Right hand</label>
+        <label
+          ><input
+            v-model="question.handedness"
+            value="left"
+            type="radio"
+            :name="question.name"
+          />Left hand</label
+        >
+        <label
+          ><input
+            v-model="question.handedness"
+            value="noPref"
+            type="radio"
+            :name="question.name"
+          />No preference</label
+        >
+        <label
+          ><input
+            v-model="question.handedness"
+            value="right"
+            type="radio"
+            :name="question.name"
+          />Right hand</label
+        >
       </div>
       <div class="label-group">
-        <label><input type="checkbox" />I never use the other hand</label>
-      </div>
-    </div>
-    <div class="question">
-      <p>Which hand do you use to draw?</p>
-      <div class="label-group">
-        <label><input type="radio" />Left hand</label>
-        <label><input type="radio" />No preference</label>
-        <label><input type="radio" />Right hand</label>
-      </div>
-      <div class="label-group">
-        <label><input type="checkbox" />I never use the other hand</label>
-      </div>
-    </div>
-    <div class="question">
-      <p>Which hand do you use to throw?</p>
-      <div class="label-group">
-        <label><input type="radio" />Left hand</label>
-        <label><input type="radio" />No preference</label>
-        <label><input type="radio" />Right hand</label>
-      </div>
-      <div class="label-group">
-        <label><input type="checkbox" />I never use the other hand</label>
-      </div>
-    </div>
-    <div class="question">
-      <p>Which hand do you use when using scissors?</p>
-      <div class="label-group">
-        <label><input type="radio" />Left hand</label>
-        <label><input type="radio" />No preference</label>
-        <label><input type="radio" />Right hand</label>
-      </div>
-      <div class="label-group">
-        <label><input type="checkbox" />I never use the other hand</label>
+        <label
+          ><input v-model="question.useOtherHand" type="checkbox" />I never use
+          the other hand</label
+        >
       </div>
     </div>
-    <div class="question">
-      <p>Which hand do you use when using a toothbrush?</p>
-      <div class="label-group">
-        <label><input type="radio" />Left hand</label>
-        <label><input type="radio" />No preference</label>
-        <label><input type="radio" />Right hand</label>
-      </div>
-      <div class="label-group">
-        <label><input type="checkbox" />I never use the other hand</label>
-      </div>
-    </div>
-    <div class="question">
-      <p>Which hand do you use when using a knife (without a fork)?</p>
-      <div class="label-group">
-        <label><input type="radio" />Left hand</label>
-        <label><input type="radio" />No preference</label>
-        <label><input type="radio" />Right hand</label>
-      </div>
-      <div class="label-group">
-        <label><input type="checkbox" />I never use the other hand</label>
-      </div>
-    </div>
-    <div class="question">
-      <p>Which hand do you use when using a spoon?</p>
-      <div class="label-group">
-        <label><input type="radio" />Left hand</label>
-        <label><input type="radio" />No preference</label>
-        <label><input type="radio" />Right hand</label>
-      </div>
-      <div class="label-group">
-        <label><input type="checkbox" />I never use the other hand</label>
-      </div>
-    </div>
-    <div class="question">
-      <p>Which hand do you use when using a broom (upper hand)?</p>
-      <div class="label-group">
-        <label><input type="radio" />Left hand</label>
-        <label><input type="radio" />No preference</label>
-        <label><input type="radio" />Right hand</label>
-      </div>
-      <div class="label-group">
-        <label><input type="checkbox" />I never use the other hand</label>
-      </div>
-    </div>
-    <div class="question">
-      <p>Which hand do you use to strike a match?</p>
-      <div class="label-group">
-        <label><input type="radio" />Left hand</label>
-        <label><input type="radio" />No preference</label>
-        <label><input type="radio" />Right hand</label>
-      </div>
-      <div class="label-group">
-        <label><input type="checkbox" />I never use the other hand</label>
-      </div>
-    </div>
-    <div class="question">
-      <p>Which hand do you use to open a box (holding the lid)?</p>
-      <div class="label-group">
-        <label><input type="radio" />Left hand</label>
-        <label><input type="radio" />No preference</label>
-        <label><input type="radio" />Right hand</label>
-      </div>
-      <div class="label-group">
-        <label><input type="checkbox" />I never use the other hand</label>
-      </div>
-    </div>
+
     <p class="error">Test</p>
-    <p class="success">Test</p>
     <button @click.prevent="save">Continue</button>
   </form>
 </template>
 
 <script setup lang="ts">
+import type { LateralityQuestion } from '@/utils/types/laterality'
+import { ref } from 'vue'
+
 const emit = defineEmits(['finishedTest'])
 function save() {
   emit('finishedTest')
 }
+
+const lateralityQuestions = ref<LateralityQuestion[]>([
+  {
+    name: 'write',
+    question: 'Which hand do you use to write?',
+    handedness: '',
+    useOtherHand: null,
+  },
+  {
+    name: 'draw',
+    question: 'Which hand do you use to draw?',
+    handedness: '',
+    useOtherHand: null,
+  },
+  {
+    name: 'throw',
+    question: 'Which hand do you use to throw?',
+    handedness: '',
+    useOtherHand: null,
+  },
+  {
+    name: 'scissors',
+    question: 'Which hand do you use when using scissors?',
+    handedness: '',
+    useOtherHand: null,
+  },
+  {
+    name: 'toothbrush',
+    question: 'Which hand do you use when using a toothbrush?',
+    handedness: '',
+    useOtherHand: null,
+  },
+  {
+    name: 'knife',
+    question: 'Which hand do you use when using a knife (without a fork)?',
+    handedness: '',
+    useOtherHand: null,
+  },
+  {
+    name: 'spoon',
+    question: 'Which hand do you use when using a spoon?',
+    handedness: '',
+    useOtherHand: null,
+  },
+  {
+    name: 'broom',
+    question: 'Which hand do you use when using a broom (upper hand)?',
+    handedness: '',
+    useOtherHand: null,
+  },
+  {
+    name: 'match',
+    question: 'Which hand do you use to strike a match?',
+    handedness: '',
+    useOtherHand: null,
+  },
+  {
+    name: 'lid',
+    question: 'Which hand do you use to open a box (holding the lid)?',
+    handedness: '',
+    useOtherHand: null,
+  },
+])
 </script>
---
+++ thesis-experiment/src/utils/types/laterality.ts
@@ -0,0 +1,6 @@
+export interface LateralityQuestion {
+  name: string
+  question: string
+  handedness: string
+  useOtherHand: boolean | null
+}

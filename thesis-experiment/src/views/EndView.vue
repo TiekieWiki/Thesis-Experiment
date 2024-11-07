f235afd628d649b169e98ef7c53abe7af8cb5d0b
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-11-07 15:33:10 +0100

    Created end page

--- thesis-experiment/src/views/EndView.vue
+++ thesis-experiment/src/views/EndView.vue
@@ -1,5 +1,11 @@
 <template>
-  <main class="end"></main>
+  <main class="end">
+    <h1>Thank you for participating!</h1>
+    <p>
+      You have completed the experiment and your answers have been saved. Thank
+      you for your time.
+    </p>
+  </main>
 </template>
 
 <script setup lang="ts"></script>

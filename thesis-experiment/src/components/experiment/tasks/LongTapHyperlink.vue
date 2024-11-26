6d978219ae287672e9400dbef872750afa0e5234
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2024-11-26 17:47:37 +0100

    Double tap zoom out measurements

--- thesis-experiment/src/components/experiment/tasks/LongTapHyperlink.vue
+++ thesis-experiment/src/components/experiment/tasks/LongTapHyperlink.vue
@@ -13,6 +13,8 @@
     </teleport>
   </div>
   <button @click="next">Next</button>
+  <div id="firstDiv"></div>
+  <div id="secondDiv"></div>
 </template>
 
 <script setup lang="ts">
@@ -33,4 +35,34 @@ const popUpOpen = ref<boolean>(false)
 function openPopUp() {
   popUpOpen.value = true
 }
+
+// const firstDiv = document.getElementById('firstDiv')
+
+// // Define coordinates in pixels
+// const x1 = currentAction.value.centerX // Horizontal position in pixels
+// const y1 = currentAction.value.centerY // Vertical position in pixels
+
+// // Apply coordinates to the div
+// firstDiv!.style.left = x1 + 'px'
+// firstDiv!.style.top = y1 + 'px'
+
+// const secondDiv = document.getElementById('secondDiv')
+
+// // Define coordinates in pixels
+// const x2 = goalAction.value.centerX // Horizontal position in pixels
+// const y2 = goalAction.value.centerY // Vertical position in pixels
+
+// // Apply coordinates to the div
+// secondDiv!.style.left = x2 + 'px'
+// secondDiv!.style.top = y2 + 'px'
 </script>
+
+<style scoped>
+#firstDiv,
+#secondDiv {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  background-color: red;
+}
+</style>

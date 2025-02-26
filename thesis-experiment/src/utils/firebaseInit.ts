6bb7564c21a8c58f4dab958164f338e789dfd88c
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2025-02-26 15:30:11 +0100

    Move firebaseConfig to separate file

--- thesis-experiment/src/utils/firebaseInit.ts
+++ thesis-experiment/src/utils/firebaseInit.ts
@@ -1,14 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyDeo-uWuMSReFVzjCpdBRYAzveuHk-mnQg',
-  authDomain: 'thesis-experiment.firebaseapp.com',
-  projectId: 'thesis-experiment',
-  storageBucket: 'thesis-experiment.appspot.com',
-  messagingSenderId: '101734678899',
-  appId: '1:101734678899:web:634dfdc3e30834919d8506',
-};
+import { firebaseConfig } from './firebaseConfig';
 
 // Initialize Firebase
 initializeApp(firebaseConfig);

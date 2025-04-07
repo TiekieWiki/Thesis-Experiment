462dd0edee8d948560480023f8b6fd6e09d93732
Tieke Stellingwerf <tieke.stellingwerf@dimaros.nl>
2025-04-07 14:42:59 +0200

    Experiment closed page

--- thesis-experiment/src/App.vue
+++ thesis-experiment/src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <RouterView />
+  <template v-if="experimentClosed">
+    <main class="start">
+      <div class="start-instructions">
+        <h1>Experiment closed</h1>
+        <p>This experiment is currently closed for new participants.</p>
+      </div>
+    </main>
+  </template>
+  <RouterView v-else />
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const experimentClosed = ref<boolean>(false);
+</script>
